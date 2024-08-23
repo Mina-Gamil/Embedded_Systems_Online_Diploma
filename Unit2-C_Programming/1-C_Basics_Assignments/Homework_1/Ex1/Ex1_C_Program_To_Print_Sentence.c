04365563ebbc8e77068e6577ab1c7b250e71b087
Mina Gamil <minagamil.ga@gmail.com>
2024-08-24 02:03:14 +0300

    Ex1 Print Sentence

---
+++ Unit2-C_Programming/1-C_Basics_Assignments/Homework_1/Ex1/Ex1_C_Program_To_Print_Sentence.c
@@ -0,0 +1,16 @@
+/*
+ ======================================================================================================================
+ Name        : Ex1_C_Program_To_Print_Sentence.c
+ Author      : Mina Gamil
+ Created on	 : Aug 24, 2024
+ Description : C Program To Print A Sentence On Screen
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("C Programming");
+    return 0;
+}

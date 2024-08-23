da3e1a89febd86cb3285a74f7784ab1d054adf3e
Mina Gamil <minagamil.ga@gmail.com>
2024-08-24 02:17:50 +0300

    Ex2 Print an integar Entered by User

---
+++ Unit2-C_Programming/1-C_Basics_Assignments/Homework_1/Ex2/Ex2_Print_an_integer_Entered_by_a_User.c
@@ -0,0 +1,19 @@
+/*
+ ======================================================================================================================
+ Name        : Ex2_Print_an_integer_Entered_by_a_User
+ Author      : Mina Gamil
+ Created on	 : Aug 24, 2024
+ Description : Print an integar Entered by a user
+ ======================================================================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("You entered: %d", number);
+    return 0;
+}
